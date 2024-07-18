2aa216c731828df1fc2fd66b085c45c27636b905
Andrew Dettor <dettor.andrew@gmail.com>
2024-07-17 23:40:19 -0400

    edited orchestration script to have correct new directory

--- ETL_orchestration_script.bat
+++ ETL_orchestration_script.bat
@@ -1,12 +1,12 @@
 @echo off
 
 REM Change to the directory containing the scripts and files
-cd /d C:\Users\detto\Documents\YouTubeViewPrediction
+cd /d C:\Users\detto\Documents\YouTubeViewPrediction\ETLs
 
 REM Execute the Python scripts in sequence with a sleep interval
-C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe new_categories_ETL.py
-C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe new_videos_ETL.py
-C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe new_channels_ETL.py
-timeout /t 2 /nobreak
-C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe update_videos_ETL.py
-C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe update_channels_ETL.py
+C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe video_dim_ETL.py
+C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe channel_dim_ETL.py
+C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe categories_dim_ETL.py
+timeout /t 1 /nobreak
+C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe video_fact_ETL.py
+C:\Users\detto\AppData\Local\Programs\Python\Python310\python.exe channel_fact_ETL.py
